7d1e787927f69f895eae0e8b59681eb8eb8c738f
MKlinlin <3024672738@qq.com>
2023-11-10 20:56:10 +0800

    初始化项目

---
+++ src/settings.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+  title: '人力资源后台管理系统',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: true,
+  // 固定header
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: true
+}
