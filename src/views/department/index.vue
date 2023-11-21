4c22783932e7f7e42816a4078eb14bc013b5755c
MKlinlin <3024672738@qq.com>
2023-11-21 20:25:16 +0800

    添加子部门组件的创建

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -3,39 +3,46 @@
     <div class="app-container">
       <!-- 展示树形结构 -->
       <!-- 默认展开子节点 -->
-      <el-tree :default-expand-all="true" :data="depts" :props="defaultProps">
+      <el-tree :expand-on-click-node="flase" default-expand-all :data="depts" :props="defaultProps">
         <!-- 节点结构 -->
         <template v-slot="{ data }">
           <el-row style="width:100%;height:40px" type="flex" justify="space-between" align="center">
             <el-col>{{ data.name }}</el-col>
             <el-col :span="4">
               <span class="tree-manager">{{ data.managerName }}</span>
-              <el-dropdown>
+              <el-dropdown @command="operateDept">
                 <span class="el-dropdown-link">
                   操作  <i class="el-icon-arrow-down el-icon--right" />
                 </span>
                 <!-- 下拉菜单选项 -->
                 <el-dropdown-menu>
-                  <el-dropdown-item>添加子部门</el-dropdown-item>
-                  <el-dropdown-item>编辑</el-dropdown-item>
-                  <el-dropdown-item>删除</el-dropdown-item>
+                  <el-dropdown-item command="add">添加子部门</el-dropdown-item>
+                  <el-dropdown-item command="edit">编辑</el-dropdown-item>
+                  <el-dropdown-item command="delete">删除</el-dropdown-item>
                 </el-dropdown-menu>
               </el-dropdown>
             </el-col>
           </el-row>
         </template>
-
       </el-tree>
     </div>
+    <!-- 放置弹层 -->
+    <!-- 表示会接受子组件的事件 update：showDialog，值=>属性 -->
+    <add-dept :show-dialog.sync="showDialog" />
   </div>
 </template>
 <script>
 import { getDepartment } from '@/api/department' // 导入获取部门数据的接口
 import { transListToTreeData } from '@/utils/index' // 导入工具函数
+import AddDept from './components/add-dept.vue' // 导入添加部门的组件
 export default {
   name: 'Department',
+  components: {
+    AddDept
+  },
   data() {
     return {
+      showDialog: false, // 控制弹层的显示和隐藏
       depts: [], // 部门数据
       defaultProps: {
         children: 'children', // 读取子节点的字段名
@@ -51,6 +58,11 @@ export default {
     async getDepartment() {
       const result = await getDepartment()
       this.depts = transListToTreeData(result, 0)
+    },
+    operateDept(type) {
+      if (type === 'add') {
+        this.showDialog = true
+      }
     }
   }
 }
@@ -62,10 +74,9 @@ app-container {
   font-size: 14px;
 }
 .tree-manager {
-  color: #409EFF;
   font-size: 12px;
   display: inline-block;
   width: 50px;
-  margin:10px;
+  margin:20px;
 }
 </style>
