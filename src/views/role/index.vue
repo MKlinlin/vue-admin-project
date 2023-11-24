03d351bf2a7e4ece9492dda5f0a16d1c98ab6e36
MKlinlin <3024672738@qq.com>
2023-11-24 12:11:17 +0800

    完成角色管理页面结构

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -1,12 +1,29 @@
 <template>
   <div class="container">
     <div class="app-container">
-      角色管理
-    </div>
-  </div>
-</template>
+      <!-- 角色管理内容 -->
+      <div class="role-operate">
+        <el-button type="primary" size="mini">添加角色</el-button>
+      </div>
+      <el-table>
+        <!-- 放置列 -->
+        <el-table-column align="center" width="200" label="角色" />
+        <el-table-column align="center" width="200" label="启用 " />
+        <el-table-column align="center" label="描述" />
+        <el-table-column align="center" label="操作" />
+      </el-table>
+      <!-- 分页组件 -->
+      <el-row type="flex" style="height:60px" align="middle" justify="end">
+        <el-pagination layout="prev, pager, next" />
+      </el-row>
+    </div></div></template>
 <script>
 export default {
   name: 'Role'
 }
 </script>
+<style>
+.role-operate{
+  padding: 10px;
+}
+  </style>
