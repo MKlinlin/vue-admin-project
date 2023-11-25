3d245a4a052465a4ac17e5ac83308904bc700a5f
Junshan Lu <3024672738@qq.com>
2023-11-25 14:28:19 +0800

    实现删除角色功能

--- src/api/role.js
+++ src/api/role.js
@@ -30,3 +30,13 @@ export function updateRole(data) {
     data
   })
 }
+
+/** *
+ * 删除角色
+ * **/
+export function deleteRole(id) {
+  return request({
+    url: `/sys/role/${id}`,
+    method: 'delete'
+  })
+}
