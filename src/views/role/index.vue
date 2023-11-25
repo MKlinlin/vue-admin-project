b563f1383df0b2fd6167607c149a66dbff67d332
Junshan Lu <3024672738@qq.com>
2023-11-25 11:12:56 +0800

    添加角色弹层表单校验

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -3,7 +3,7 @@
     <div class="app-container">
       <!-- 角色管理内容 -->
       <div class="role-operate">
-        <el-button @click="showDialog=true " type="primary" size="mini">添加角色</el-button>
+        <el-button type="primary" size="mini" @click="showDialog=true ">添加角色</el-button>
       </div>
       <el-table :data="list">
         <!-- 放置列 -->
@@ -35,30 +35,28 @@
         />
       </el-row>
     </div>
-  <!-- 放置弹层 -->
-  <el-dialog title="添加角色" :visible.sync="showDialog" width="500px">
-    <el-form label-width="120px">
-      <el-form-item label="角色名称">
-        <el-input size="mini" style="width:300px" placeholder="请输入角色名称"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-switch size="mini" />
-      </el-form-item>
-      <el-form-item label="角色描述">
-        <el-input type="textarea" :rows="3" size="mini" style="width:300px" placeholder="请输入角色描述">
-
-        </el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-row>
-          <el-col :span="12">
-            <el-button type="primary" size="mini">确定</el-button>
-            <el-button size="mini" @click="showDialog = false">取消</el-button>
-          </el-col>
-        </el-row>
-      </el-form-item>
-    </el-form>
-  </el-dialog>
+    <!-- 放置弹层 -->
+    <el-dialog title="添加角色" :visible.sync="showDialog" width="500px">
+      <el-form ref="roleForm" :rules="rules" :model="roleForm" label-width="120px">
+        <el-form-item prop="name" label="角色名称">
+          <el-input v-model="roleForm.name" size="mini" style="width:300px" placeholder="请输入角色名称" />
+        </el-form-item>
+        <el-form-item label="启用">
+          <el-switch v-model="roleForm.state" :active-value="1" :inactive-value="0" size="mini" />
+        </el-form-item>
+        <el-form-item prop="description" label="角色描述">
+          <el-input v-model="roleForm.description" type="textarea" :rows="3" size="mini" style="width:300px" placeholder="请输入角色描述" />
+        </el-form-item>
+        <el-form-item>
+          <el-row>
+            <el-col :span="12">
+              <el-button type="primary" size="mini">确定</el-button>
+              <el-button size="mini" @click="showDialog = false">取消</el-button>
+            </el-col>
+          </el-row>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div></template>
 <script>
 import { getRoleList } from '@/api/role'
@@ -73,6 +71,15 @@ export default {
         page: 1, // 当前页
         pagesize: 5, // 每页显示条数
         total: 0 // 总条数
+      },
+      roleForm: {
+        name: '',
+        description: '',
+        state: 0 // 默认未启用
+      },
+      rules: {
+        name: [{ required: true, message: '角色名称不能为空', trigger: 'blur' }],
+        description: [{ required: true, message: '角色描述不能为空', trigger: 'blur' }]
       }
     }
   },
