83836b5a15b3539b18af89867e93fd5f3b0bbebf
MKlinlin <3024672738@qq.com>
2023-11-29 23:40:18 +0800

    完成员工导入模板

--- src/api/employee.js
+++ src/api/employee.js
@@ -20,3 +20,13 @@ export function exportEmployee() {
     responseType: 'blob' // 二进制流
   })
 }
+/**
+ * 下载员工导入模板
+ * */
+export function getExportTemplate() {
+  return request({
+    url: '/sys/user/import/template',
+    // 改变接受数据的类型
+    responseType: 'blob' // 二进制流
+  })
+}
